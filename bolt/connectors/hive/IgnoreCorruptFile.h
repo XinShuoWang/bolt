c1df44e4948e9e40de69a9f2cdb496245da0e80b
wangxinshuo.db <wangxinshuo.db@bytedance.com>
2026-01-16 16:45:18 +0800

    fix

--- bolt/connectors/hive/IgnoreCorruptFile.h
+++ bolt/connectors/hive/IgnoreCorruptFile.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) ByteDance Ltd. and/or its affiliates
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,25 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* --------------------------------------------------------------------------
- * Copyright (c) 2025 ByteDance Ltd. and/or its affiliates.
- * SPDX-License-Identifier: Apache-2.0
- *
- * This file has been modified by ByteDance Ltd. and/or its affiliates on
- * 2025-11-11.
- *
- * Original file was released under the Apache License 2.0,
- * with the full license text available at:
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * This modified file is released under the same license.
- * --------------------------------------------------------------------------
- */
 
 #pragma once
-
-#include "bolt/common/base/Exceptions.h"
-#include "bolt/connectors/hive/storage_adapters/hdfs/StorageException.h"
 
 #include <fmt/core.h>
 #include <folly/Format.h>
@@ -42,6 +25,9 @@
 #include <set>
 #include <string>
 #include <vector>
+
+#include "bolt/common/base/Exceptions.h"
+#include "bolt/connectors/hive/storage_adapters/hdfs/StorageException.h"
 
 class IgnoreCorruptFileHelper {
  public:
